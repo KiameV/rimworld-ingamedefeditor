75efdaf426bf6cbd15f85d0389b845f96f00c192
Travis Offtermatt <travisty2@gmail.com>
2018-12-09 12:04:44 -0700

    Redesigning main window def selection

--- Source/Gui/Window.cs
+++ Source/Gui/Window.cs
@@ -10,7 +10,8 @@ namespace InGameDefEditor
 {
     class InGameDefEditorWindow : Window
 	{
-		private IParentStatWidget selected = null;
+		private IEditableDefType selectedDefType = null;
+		private IParentStatWidget selectedDef = null;
 
         private Vector2 leftScroll = Vector2.zero,
                         middleScroll = Vector2.zero,
@@ -19,58 +20,53 @@ namespace InGameDefEditor
         private float previousYMaxLeft = 0,
                       previousYMaxMiddle = 0,
                       previousYMaxRight = 0;
-
-        private readonly List<IButtonWidget> buttons;
-
-        public override Vector2 InitialSize => new Vector2((float)UI.screenWidth - 20, (float)UI.screenHeight - 20);
+		
+		public override Vector2 InitialSize => new Vector2((float)UI.screenWidth - 20, (float)UI.screenHeight - 20);
 
         public InGameDefEditorWindow()
         {
             IOUtil.LoadData();
-			buttons = new List<IButtonWidget>()
-			{
-				new ButtonWidget<ThingDef>("Apparel", DefType.Apparel, Defs.ApparelDefs.Values, this.CreateSelected),
-				new ButtonWidget<ThingDef>("Weapons", DefType.Weapon, Defs.WeaponDefs.Values, this.CreateSelected),
-				new ButtonWidget<ThingDef>("Projectiles", DefType.Projectile, Defs.ProjectileDefs.Values, this.CreateSelected),
-				new ButtonWidget<BiomeDef>("Biomes", DefType.Biome, Defs.BiomeDefs.Values, this.CreateSelected),
-				new ButtonWidget<TraitDef>("Traits", DefType.Trait, Defs.TraitDefs.Values, this.CreateSelected),
-				new ButtonWidget<ThoughtDef>("Thoughts", DefType.Thought, Defs.ThoughtDefs.Values, this.CreateSelected),
-				new ButtonWidget<StorytellerDef>("Story Tellers", DefType.StoryTeller, Defs.StoryTellerDefs.Values, this.CreateSelected),
-				new ButtonWidget<DifficultyDef>("Difficulty", DefType.Difficulty, this.SortDifficultyOptions(Defs.DifficultyDefs.Values), this.CreateSelected),
-				new ButtonWidget<ThingDef>("Ingestible", DefType.Ingestible, Defs.IngestibleDefs.Values, this.CreateSelected),
-				new ButtonWidget<ThingDef>("Mineable", DefType.Mineable, Defs.MineableDefs.Values, this.CreateSelected),
-				new ButtonWidget<RecipeDef>("Recipes", DefType.Recipe, Defs.RecipeDefs.Values, this.CreateSelected),
-			};
-
-			//if (Controller.EnableRecipes)
-			buttons.Add(new ButtonWidget<ThingDef>("Disabled", DefType.Disabled, Defs.DisabledThingDefs.Values, this.CreateSelected));
         }
 
 		public override void DoWindowContents(Rect rect)
         {
             Text.Font = GameFont.Small;
-            float outerY = 0;
+            float outerY = 10;
 
-            float x = 10;
-            float buttonX = x;
-            foreach (var w in this.buttons)
-            {
-                w.Draw(buttonX, outerY, 150, this.selected);
-                buttonX += 160;
+			if (Widgets.ButtonText(new Rect(10, outerY, 200, 30), ((this.selectedDefType == null) ? "Def Type" : this.selectedDefType.Label)))
+			{
+				WindowUtil.DrawFloatingOptions(
+                        new WindowUtil.FloatOptionsArgs<IEditableDefType>()
+                        {
+                            items = this.GetDefTypes(true),
+                            getDisplayName = dt => dt.Label,
+							onSelect = dt =>
+							{
+								this.selectedDefType = dt;
+								this.selectedDef = null;
+							}
+						});
+			}
 
-                if (buttonX + 150 > rect.xMax)
-                {
-                    buttonX = 10;
-                    outerY += 40;
-                }
-            }
+			if (this.selectedDefType != null)
+			{
+				if (Widgets.ButtonText(new Rect(220, outerY, 200, 30), ((this.selectedDef == null) ? this.selectedDefType.Label + " Def" : Util.GetDefLabel(this.selectedDef.BaseDef))))
+				{
+					WindowUtil.DrawFloatingOptions(new WindowUtil.FloatOptionsArgs<Def>()
+					{
+						items = this.selectedDefType.GetDefs(),
+						getDisplayName = def => Util.GetDefLabel(def),
+						onSelect = def => CreateSelected(def, this.selectedDefType.Type)
+					});
+				}
+			}
             
             outerY += 60;
 
-            if (this.selected != null)
+            if (this.selectedDef != null)
             {
-                float y = 0;
-                x = 0;
+				float x = 0;
+				float y = 0;
 
                 // Left column
                 Widgets.BeginScrollView(
@@ -78,7 +74,7 @@ namespace InGameDefEditor
                     ref this.leftScroll,
                     new Rect(0, 0, 354, this.previousYMaxLeft));
 
-                this.selected.DrawLeft(x, ref y, 354);
+                this.selectedDef.DrawLeft(x, ref y, 354);
                 this.previousYMaxLeft = y;
 
                 Widgets.EndScrollView();
@@ -89,7 +85,7 @@ namespace InGameDefEditor
                     ref this.middleScroll,
                     new Rect(0, 0, 354, this.previousYMaxMiddle));
                 y = 0;
-                this.selected.DrawMiddle(x, ref y, 354);
+                this.selectedDef.DrawMiddle(x, ref y, 354);
                 this.previousYMaxMiddle = y;
                 Widgets.EndScrollView();
 
@@ -99,14 +95,14 @@ namespace InGameDefEditor
                     ref this.rightScroll,
                     new Rect(0, 0, 354, this.previousYMaxRight));
                 y = 0;
-                this.selected.DrawRight(x, ref y, 354);
+                this.selectedDef.DrawRight(x, ref y, 354);
                 this.previousYMaxRight = y;
 
                 Widgets.EndScrollView();
 
                 if (Widgets.ButtonText(new Rect(rect.xMax - 340, rect.yMax - 32, 100, 32), "Reset".Translate()))
                 {
-                    Find.WindowStack.Add(Dialog_MessageBox.CreateConfirmation("Reset " + this.selected.DisplayLabel + "?", delegate { this.ResetSelected(); }));
+                    Find.WindowStack.Add(Dialog_MessageBox.CreateConfirmation("Reset " + this.selectedDef.DisplayLabel + "?", delegate { this.ResetSelected(); }));
                 }
             }
 
@@ -115,10 +111,10 @@ namespace InGameDefEditor
                 Find.WindowStack.TryRemove(typeof(InGameDefEditorWindow), true);
             }
 
-			if (this.selected != null &&
-				Widgets.ButtonText(new Rect(rect.xMax - 230, rect.yMax - 32, 120, 32), "Reset".Translate() + " " + this.selected.Type))
+			if (this.selectedDef != null &&
+				Widgets.ButtonText(new Rect(rect.xMax - 230, rect.yMax - 32, 120, 32), "Reset".Translate() + " " + this.selectedDef.Type))
 			{
-				Find.WindowStack.Add(Dialog_MessageBox.CreateConfirmation("Reset".Translate() + " " + this.selected.Type + "?", delegate { this.ResetDefType(this.selected.Type); }));
+				Find.WindowStack.Add(Dialog_MessageBox.CreateConfirmation("Reset".Translate() + " " + this.selectedDef.Type + "?", () => { this.selectedDefType.ResetTypeDefs(); this.selectedDef?.ResetBuffers(); }));
 			}
 
             if (Widgets.ButtonText(new Rect(rect.xMax - 100, rect.yMax - 32, 100, 32), "InGameDefEditor.ResetAll".Translate()))
@@ -127,34 +123,20 @@ namespace InGameDefEditor
                     "Reset everything to the original game's settings?",
                     delegate
                     {
-						foreach (var v in this.buttons)
+						foreach (var v in this.GetDefTypes(false))
 							v.ResetTypeDefs();
-						if (this.selected != null)
-							this.selected.Rebuild();
+						if (this.selectedDef != null)
+							this.selectedDef.Rebuild();
 					}));
             }
         }
 
 		private void ResetSelected()
 		{
-			if (this.selected != null)
+			if (this.selectedDef != null)
 			{
-				Backup.ApplyStats(this.selected.BaseDef);
-				this.selected.Rebuild();
-			}
-		}
-
-		private void ResetDefType(DefType type)
-		{
-			foreach (var v in this.buttons)
-			{
-				if (v.Type == type)
-				{
-					v.ResetTypeDefs();
-					if (this.selected != null)
-						this.selected.Rebuild();
-					break;
-				}
+				Backup.ApplyStats(this.selectedDef.BaseDef);
+				this.selectedDef.Rebuild();
 			}
 		}
 
@@ -178,28 +160,28 @@ namespace InGameDefEditor
 				case DefType.Mineable:
 				case DefType.Ingestible:
 				case DefType.Disabled:
-					this.selected = new ThingDefWidget(d as ThingDef, type);
+					this.selectedDef = new ThingDefWidget(d as ThingDef, type);
                     break;
 				case DefType.Projectile:
-					this.selected = new ProjectileDefWidget(d as ThingDef, type);
+					this.selectedDef = new ProjectileDefWidget(d as ThingDef, type);
 					break;
 				case DefType.Biome:
-                    this.selected = new BiomeWidget(d as BiomeDef, type);
+                    this.selectedDef = new BiomeWidget(d as BiomeDef, type);
                     break;
 				case DefType.Recipe:
-					this.selected = new RecipeWidget(d as RecipeDef, type);
+					this.selectedDef = new RecipeWidget(d as RecipeDef, type);
 					break;
 				case DefType.Trait:
-					this.selected = new TraitWidget(d as TraitDef, type);
+					this.selectedDef = new TraitWidget(d as TraitDef, type);
 					break;
 				case DefType.Thought:
-					this.selected = new ThoughtDefWidget(d as ThoughtDef, type);
+					this.selectedDef = new ThoughtDefWidget(d as ThoughtDef, type);
 					break;
 				case DefType.StoryTeller:
-					this.selected = new StoryTellerDefWidget(d as StorytellerDef, type);
+					this.selectedDef = new StoryTellerDefWidget(d as StorytellerDef, type);
 					break;
 				case DefType.Difficulty:
-					this.selected = new DifficultyDefWidget(d as DifficultyDef, type);
+					this.selectedDef = new DifficultyDefWidget(d as DifficultyDef, type);
 					break;
 			}
             this.ResetScrolls();
@@ -226,7 +208,68 @@ namespace InGameDefEditor
 		}
 
 		#region ButonWidget
-		private interface IButtonWidget
+		private interface IEditableDefType
+		{
+			DefType Type { get; }
+			string Label { get; }
+
+			IEnumerable<Def> GetDefs();
+			void ResetTypeDefs();
+		}
+		private struct EditableDefType<D> : IEditableDefType where D : Def, new()
+		{
+			private readonly string label;
+			public readonly DefType type;
+			public readonly IEnumerable<D> Defs;
+			
+			public string Label => this.label;
+			public DefType Type => this.type;
+
+			public EditableDefType(string label, DefType type, IEnumerable<D> defs)
+			{
+				this.label = label;
+				this.type = type;
+				this.Defs = defs;
+			}
+
+			public IEnumerable<Def> GetDefs()
+			{
+				List<Def> l = new List<Def>();
+				foreach (Def d in this.Defs)
+					l.Add(d);
+				return l;
+			}
+
+			public void ResetTypeDefs()
+			{
+				foreach (var v in this.Defs)
+					Backup.ApplyStats(v);
+			}
+		}
+
+		private IEnumerable<IEditableDefType> GetDefTypes(bool includeDisabled)
+		{
+			List<IEditableDefType> defTypes = new List<IEditableDefType>()
+			{
+				new EditableDefType<ThingDef>("Apparel", DefType.Apparel, Defs.ApparelDefs.Values),
+				new EditableDefType<ThingDef>("Weapons", DefType.Weapon, Defs.WeaponDefs.Values),
+				new EditableDefType<ThingDef>("Projectiles", DefType.Projectile, Defs.ProjectileDefs.Values),
+				new EditableDefType<BiomeDef>("Biomes", DefType.Biome, Defs.BiomeDefs.Values),
+				new EditableDefType<TraitDef>("Traits", DefType.Trait, Defs.TraitDefs.Values),
+				new EditableDefType<ThoughtDef>("Thoughts", DefType.Thought, Defs.ThoughtDefs.Values),
+				new EditableDefType<StorytellerDef>("Story Tellers", DefType.StoryTeller, Defs.StoryTellerDefs.Values),
+				new EditableDefType<DifficultyDef>("Difficulty", DefType.Difficulty, this.SortDifficultyOptions(Defs.DifficultyDefs.Values)),
+				new EditableDefType<ThingDef>("Ingestible", DefType.Ingestible, Defs.IngestibleDefs.Values),
+				new EditableDefType<ThingDef>("Mineable", DefType.Mineable, Defs.MineableDefs.Values),
+				new EditableDefType<RecipeDef>("Recipes", DefType.Recipe, Defs.RecipeDefs.Values),
+			};
+
+			if (includeDisabled && Defs.DisabledThingDefs.Count > 0)
+				defTypes.Add(new EditableDefType<ThingDef>("Disabled", DefType.Disabled, Defs.DisabledThingDefs.Values));
+			return defTypes;
+		}
+
+		/*private interface IButtonWidget
         {
 			DefType Type { get; }
             void Draw(float x, float y, float width, IParentStatWidget selected);
@@ -275,7 +318,7 @@ namespace InGameDefEditor
 				foreach (var v in this.possibleDefs)
 					Backup.ApplyStats(v);
 			}
-		}
-        #endregion
-    }
+		}*/
+		#endregion
+	}
 }
